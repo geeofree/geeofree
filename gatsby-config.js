22ed315163777bf90dd81798fa22d40aa248a5e8
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2021-09-25 23:18:44 +0800

    Implement home layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,34 @@ module.exports = {
     "gatsby-plugin-styled-components",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-mdx",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Geeofree`,
+        short_name: `Geeofree`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#172B4D`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
         path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Zilla Slab", "Zilla Slab:bold"],
+        },
       },
       __key: "pages",
     },
