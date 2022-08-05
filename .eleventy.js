784756814d3ad2d81160eb614e3049625e892acd
Geo Galagaran <geo@email.com>
2022-08-06 07:58:37 +0800

    Fix parent navigation sorting

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,11 @@ module.exports = (eleventyConfig) => {
 	eleventyConfig.addPassthroughCopy({ 'src/css': 'css' })
 	eleventyConfig.addPassthroughCopy({ 'src/js': 'js' })
 	eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
+
+  eleventyConfig.addCollection('parents', collection => {
+    return collection.getAll().slice().sort((a, b) => a.data.order - b.data.order)
+  })
+
 	return {
 		dir: {
 			input: path.resolve(__dirname, 'src'),
