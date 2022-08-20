1dddd0e9206fbbcdc1458426532e7fd9467ef087
Geo Galagaran <17492631+geeofree@users.noreply.github.com>
2022-08-21 07:15:30 +0800

    Install and setup syntax highlighting using PrismJS

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,22 @@
 const path = require('path')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = (eleventyConfig) => {
+  // Plugins
+  eleventyConfig.addPlugin(syntaxHighlight)
+
+  // Passthroughs
 	eleventyConfig.addPassthroughCopy({ 'src/css': 'css' })
 	eleventyConfig.addPassthroughCopy({ 'src/js': 'js' })
 	eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
 
-  eleventyConfig.addCollection('parents', collection => {
-    return collection.getAll().slice().sort((a, b) => a.data.order - b.data.order)
-  })
+  // Collections
+  eleventyConfig.addCollection('parents', collection => (
+    collection
+    .getFilteredByTag('parents')
+    .slice()
+    .sort((a, b) => a.data.order - b.data.order)
+  ))
 
 	return {
 		dir: {
